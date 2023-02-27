71bffa37497a15894730f58e19675d714f602177
admin-Komputer\admin <wagulec123@gmail.com>
2023-02-27 21:49:15 +0100

    connect with mangodb

---
+++ server.js
@@ -0,0 +1,27 @@
+const express =  require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyPraser = require('body-parser');
+
+mongoose.connect('mongodb+srv://admin:admin@kurs.hwy4owx.mongodb.net/?retryWrites=true&w=majority');
+const db= mongoose.connection;
+
+db.on('error',(err)=>{
+    console.log(err);
+})
+
+db.once('open',()=>{
+    console.log("connect dataBase");
+})
+
+const app=express()
+
+app.use(morgan('dev'));
+app.use(bodyPraser.urlencoded({extended:true}));
+app.use(bodyPraser.json())
+
+const PORT = process.env.port || 3000
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
